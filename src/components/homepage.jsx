8edac8268ee60c7e549697671e92e8a85f95f4fc
Abhi Mondal <85216013+AbhiMondal20@users.noreply.github.com>
2024-07-23 19:41:12 +0530

    Initial commit

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -13,7 +13,7 @@ export default function SpotlightPreview() {
   };
   return (
     <div>
-      <section className="relative" style={sectionStyle}>
+      <section className="relative -z-1" style={sectionStyle}>
         <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
         <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
           <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
